db9a4db9ee0950d19a80b5bd2186cfb59a633afa
Rex <rexkimta@gmail.com>
2013-12-12 14:36:32 +0800

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,6 +31,17 @@ describe "StaticPages" do
 	end
       end
 
+      describe "follower/following counts" do
+	let(:other_user) { FactoryGirl.create(:user) }
+	before do
+	  other_user.follow!(user)
+	  visit root_path
+	end
+
+	it { should have_link("0 following", href: following_user_path(user)) }
+	it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+
       it "should show how many microposts the user has" do
 	expect(page).to have_selector("aside span", text: "2 microposts")
       end
